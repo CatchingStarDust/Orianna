34712b47acdad76e3b63e07bfb71b1fb30d2f77e
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2024-05-06 02:03:40 -0700

    created first ping message bot is online

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        // Add other intents here
+    ],
+});
+
+//this initializes an event listener
+client.on('ready', (c) => {
+    console.log('We go.')
+});
+
+
+client.on('messageCreate', (message) => {
+    //if the person sending the message is a bot, DON'T DO SHIT!!!!
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+});
+
+client.login();
