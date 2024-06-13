ebbf572d9894ae516a937919db3e96184d53bddb
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2024-06-12 23:43:17 -0700

    added check for daily ammount

--- src/commands/Role Capsules/daily.js
+++ src/commands/Role Capsules/daily.js
@@ -1,3 +1,4 @@
+const {ApplicationCommandOptionType } = require('discord.js')
 const UserProfile = require('../../schemas/UserProfile');
 
 const dailyAmmount = 1;
@@ -7,8 +8,7 @@ module.exports = {
         // make sure people can only use this in the server
         if (!interaction.inGuild()) {
             interaction.reply({ content: `${needServerEmbed}`, ephemeral: true });
-            return;
-        }
+            return;}
 
         interaction.reply(`collecting dalies.`)
 
@@ -17,14 +17,33 @@ module.exports = {
 
             let userProfile = await UserProfile.findOne({userId: interaction.member.id,});
 
-        } catch (error) {
-            console.log(`Error handling / daily: ${error}`);
+// Check to see if member already collected their Capsule for the day
+            if (userProfile) {
+                const lastDailyDate = userProfile.lastDailyCollected?.toDateString();
+                const currentDate = new Date().toDateString();
 
-        }
+                if (lastDailyDate === currentDate) {
+                    interaction.reply(`You have already collected your reward for the day. Come back tomorrow.`);
+                    return;}
+
+            }else {
+                userProfile = new UserProfile ({
+                    userId: interaction.member.id,});
+            }
+
+        userProfile.balance += dailyAmmount;
+        userProfile.lastDailyCollected = new Date();
+
+        await userProfile.save();
+        
+        interaction.reply(
+            `${dailyAmmount} was added to your inventory!\nYou have ${userProfile.balance} capsule(s)`);
+
+        } catch (error) {
+            console.log(`Error handling / daily: ${error}`);}
     },
 
     data: {
         name: 'daily',
-        description: 'Gives you a capsule',
-    },
+        description: 'Gives you a capsule',},
 };
---
+++ src/commands/Role Capsules/earnCapsule.js
@@ -0,0 +1,4 @@
+const {ApplicationCommandOptionType } = require('discord.js')
+const UserProfile = require('../../schemas/UserProfile')
+
+module.exports = {}
