a9e6f9f5a522c47582dbd7f4b66c6d53f5d2f6c6
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2024-06-12 23:19:29 -0700

    changed 'daily' to 'earnedCapsule'

---
+++ src/commands/Role Capsules/earnCapsule.js
@@ -0,0 +1,12 @@
+const UserProfile = require('../../schemas/UserProfile');
+
+const dailyAmmount = 1;
+
+module.exports = {
+    run: async ({ interaction }) => {},
+
+    data: {
+        name: 'daily',
+        description: 'Gives you a capsule',
+    },
+};
