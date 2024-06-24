15025ae20322fc42c382bff8dd0d8680d563f09c
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2024-06-23 22:21:56 -0700

    fixed why deployCommands.js was not working

--- src/deployCommands.js
+++ src/deployCommands.js
@@ -4,25 +4,23 @@ console.log(process.env.TOKEN);
 const { REST, Routes } = require('discord.js');
 
 const commands = [
-	{
-		name: 'daily',
-		description: 'Gives you your daily capsule',
-	}
+    {
+        name: 'daily',
+        description: 'Gives you your daily capsule',
+    }
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-	try {
-		console.log('Registering slash commands...');
-
-		await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-			{ body: commands }
-		);
-
-		console.log('Slash commands registered.');
-	} catch (error) {
-		console.error(`There was an error: ${error}`);
-	}
+    try {
+        console.log('Registering slash commands...');
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        );
+        console.log('Slash commands registered:', data);
+    } catch (error) {
+        console.error('There was an error:', error);
+    }
 })();
