072e8d13de076269d3d9e5a2a425ca090e7b16e4
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2024-07-24 00:34:10 -0700

    created odds for basic capsule

--- src/commands/basicCapsuleGacha.js
+++ src/commands/basicCapsuleGacha.js
@@ -1,14 +1,15 @@
 const UserProfile = require('../schemas/UserProfile');
 const capsuleData = require('../schemas/capsuleData');
 
-function openBasicCapsule() {
+function basicCapsuleOdds() {
 
     //gacha that decides whether the user wins at all or not
 
     // Picks a random number
     Math.floor(Math.random() * 10) + 1;
-
-
+    
     // if user wins: Gacha for the role colour itself
 
 };
+
+module.exports = { basicCapsuleOdds, };
--- src/commands/openCapsule.js
+++ src/commands/openCapsule.js
@@ -2,6 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 const UserProfile = require('../schemas/UserProfile');
 const capsuleData = require('../schemas/capsuleData');
 const noCapsuleEmbed = require('embeds.js');
+const noCapsuleEmbed = require('embeds.js');
+const odds = require('basicCapsuleGacha.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -21,25 +23,37 @@ module.exports = {
             
             let userProfile = await UserProfile.findOne({ userId: interaction.member.id });
 
+            let userCapsules = await UserProfile.findOne({ capsules: Number });
+
+            if (userCapsules.Number = 0) {
+                await interaction.editReply(`You don't have any capsules to open.`);
+                return;
+            }
+
             userProfile.capsulesOpened = (userProfile.capsulesOpened || 0) + 1;
 
+
+            // if the user is max pity
             if (userProfile.capsulesOpened >= 100) {
 
                 // Put role colour gacha function here, since the user has reached max pity
 
-                await interaction.editReply(`CAPSULE OPENING EMBED`);
+                await interaction.editReply(`ROLE COLOUR GACHA`);
                 userProfile.capsulesOpened = 0; 
 
+                await userProfile.save();
+
+                
             } else {
 
                 // Proceed with gacha as normal
-                await interaction.editReply(`NORMAL GACHA FUNCTION`);
+                
+
+                await userProfile.save();
             }
 
             if(!userProfile) {
                 interaction.editReply(noCapsuleEmbed);
-
-                await userProfile.save();
             }
             
         } catch (error) {
