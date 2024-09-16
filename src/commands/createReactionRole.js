b1d40199b13682527a530e4af79926ebdbea60fc
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2024-09-16 01:23:52 -0700

    updated node.js

--- src/commands/createReactionRole.js
+++ src/commands/createReactionRole.js
@@ -118,6 +118,7 @@ module.exports = {
 
             //DEBUGGING
                     console.log(data); 
+                    
             //DEBUGGING
                     
              // ----------   
