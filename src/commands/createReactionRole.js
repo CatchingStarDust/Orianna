aa5fdaa31193b597cc8b349f7ddcd1d72ab755b5
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2024-09-29 02:10:43 -0700

    added checks to make sure that each emoji is attached to a correct string in database

--- src/commands/createReactionRole.js
+++ src/commands/createReactionRole.js
@@ -2,9 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('disc
 const ReactionPost = require('../schemas/roleColourData.js');
 
 
-module.exports = {
-    // the command along with the options that the admin will need to enter every time they want to add a reaction role to a message.
-    data: new SlashCommandBuilder()
+const reactionRoleSlashCommand = new SlashCommandBuilder()
         .setName('react-role')
         .setDescription('Members use this to choose colours')
         .addSubcommand(command => 
@@ -32,128 +30,103 @@ module.exports = {
                                                         // the name must be a string                                           
                                                         .setDescription('the colour name associated with the role (Names are lower-case with no spaces)')
                                                         .setRequired(true))  
-        )
-        .addSubcommand(command => 
-            command
-            .setName('remove') // same thing as above, but this is to remove the reaction data from the message id
-            .setDescription('remove a reaction role to a message')
-            .addStringOption(option => 
-                option
-                    .setName('message-id')
-                    .setDescription('the message to react to')
-                    .setRequired(true))
-                        .addStringOption(option => 
-                            option
-                                .setName('emoji')
-                                .setDescription('the emoji to react with')
-                                .setRequired(true))
-                                .addStringOption(option => 
-                                    option
-                                        .setName('colour-name')
-                                        .setDescription('the colour name associated with the role (Names are lower-case with no spaces)')
-                                        .setRequired(true)) 
-        ),
+    )
+    .addSubcommand(command => 
+        command
+        .setName('remove') // same thing as above, but this is to remove the reaction data from the message id
+        .setDescription('remove a reaction role to a message')
+        .addStringOption(option => 
+            option
+                .setName('message-id')
+                .setDescription('the message to react to')
+                .setRequired(true))
+                    .addStringOption(option => 
+                        option
+                            .setName('emoji')
+                            .setDescription('the emoji to react with')
+                            .setRequired(true))
+                            .addStringOption(option => 
+                                option
+                                    .setName('colour-name')
+                                    .setDescription('the colour name associated with the role (Names are lower-case with no spaces)')
+                                    .setRequired(true)));
 
-    async execute(interaction) {
-        const options = interaction.options;
-        const guild = interaction.guild;
-        const channel = interaction.channel;
-        const sub = options.getSubcommand();
-        const emoji = options.getString('emoji');
-        const message = await channel.messages.fetch(options.getString('message-id'), true);
-        const colourName = options.getString('colour-name'); 
 
-        //makes sure that only people who have the administrator tag can run this command
-        if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator))
-            return await interaction.reply({ content: `You don't have permission to run that!`, ephemeral: true });
+// saving a reaction role post
+const saveReactionRoleToDataBase = async (guildId, channelId, messageId, emoji, roleId, colourName) => {
+    const newReaction = new ReactionPost({
+        Guild: guildId,
+        MessageChannel: channelId,
+        Message: messageId,
+        Emoji: emoji,
+        Role: roleId,
+        ColourName: colourName,
+    });
+    await newReaction.save();
+    console.log('Reaction Role saved to DB:', newReaction);
+};
 
-       
-        const data = await ReactionPost.findOne({ 
-            Guild: guild.id, 
-            Message: message.id, 
-            Emoji: emoji, 
-            ColourName: colourName, 
-        });
+// handling the command execution
+const reactionRoleCommands = async (interaction) => {
+    const options = interaction.options;
+    const guild = interaction.guild;
+    const channel = interaction.channel;
+    const sub = options.getSubcommand();
+    const emoji = options.getString('emoji');
+    const message = await channel.messages.fetch(options.getString('message-id'), true);
+    const colourName = options.getString('colour-name'); 
 
-        let embed;  
+    const data = await ReactionPost.findOne({ 
+        Guild: guild.id, 
+        Message: message.id, 
+        Emoji: emoji, 
+        ColourName: colourName,
+    });
 
-        switch (sub) {
-            case 'add':
-                if (data) {
-                    return await interaction.reply({ content: `You already have this reaction using ${emoji} on this message`, ephemeral: true });
-                } // if the the new input match the data that has already been used on the same message id, ori stops the admin
+    let embed;
 
-            // this variable represents the option in the command that tells ori to get the role that was created in the server
-                const role = options.getRole('role');
+    switch (sub) {
+        case 'add':
+            if (data) {
+                return await interaction.reply({ content: `You already have this reaction using ${emoji} on this message`, ephemeral: true });
+            }
 
-            /* the attributes of the reaction post 
-            the admin created using the command.
-            they must have:
-            - the posts id number, 
-            - be assigned an emoiji, 
-            - attached to a role that exists in the server, 
-            - and has to be assigned a unique name */
+            const role = options.getRole('role');
+            await saveReactionRoleToDataBase(guild.id, channel.id, message.id, emoji, role.id, colourName);
 
-            const newReaction = new ReactionPost({
+            // Add reaction to the message
+            await message.react(emoji);
+
+            // Send confirmation
+            embed = new EmbedBuilder()
+                .setColor("Blurple")
+                .setDescription(`Added reaction role to [this message](${message.url}) with ${emoji}, the name "${colourName}", and the role ${role}`);
+
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+            break;
+
+        case 'remove':
+            if (!data) {
+                return await interaction.reply({ content: `That reaction role does not exist`, ephemeral: true });
+            }
+
+            await ReactionPost.deleteMany({
                 Guild: guild.id,
-                MessageChannel: channel,
                 Message: message.id,
                 Emoji: emoji,
-                Role: role.id,
                 ColourName: colourName,
             });
-          
-                await ReactionPost.findOneAndUpdate(
-                    { Guild: guild.id, Message: message.id,},
-                    { $set: {
-                        MessageChannel: channel,
-                        Message: message,
-                        Emoji: emoji, 
-                        Role: role.id, 
-                        ColourName: colourName, }},
-                    { new: true, upsert: true}
-                );
-                await newReaction.save(); 
 
-            //DEBUGGING
-                    console.log(newReaction); 
-                    console.log('Saved Emoji:', emoji);  //  the emoji you know is saved
-                    console.log('Queried Emoji:', emoji);  // The emoji you are querying
-                    
-            //DEBUGGING
-                    
-             // ----------   
-                //the embed message that confirms the post has been assigned a reaction role
-                embed = new EmbedBuilder()
-                    .setColor("Blurple")
-                    .setDescription(`Added reaction role to [this message](${message.url}) with ${emoji}, the name "${colourName}", and the role ${role}`);
+            embed = new EmbedBuilder()
+                .setColor("Blurple")
+                .setDescription(`Removed reaction role to [this message](${message.url}) with ${emoji}`);
 
-                // ori waits for a user to click on one of the reaction emojis
-                await message.react(emoji);
-
-                await interaction.reply({ embeds: [embed], ephemeral: true });
-                break;
-
-            case 'remove':
-                if (!data) {
-                    return await interaction.reply({ content: `That reaction role does not exist`, ephemeral: true });
-                }
-            
-            // same thing as the code above, but for deleting reactions from posts
-                await ReactionPost.deleteMany({
-                    Guild: guild.id,
-                    Message: message.id,
-                    Emoji: emoji,
-                    ColourName: colourName,
-                });
-
-            // confirms that the reactions were taken off the post
-                embed = new EmbedBuilder()
-                    .setColor("Blurple")
-                    .setDescription(`Removed reaction role to [this message](${message.url}) with ${emoji}`);
-
-                await interaction.reply({ embeds: [embed], ephemeral: true });
-                break;
-        }
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+            break;
     }
 };
+
+// Export the individual parts
+module.exports = { 
+    data: reactionRoleSlashCommand, 
+    execute: reactionRoleCommands, saveReactionRoleToDataBase, };
--- src/functions/assignRoleColour.js
+++ src/functions/assignRoleColour.js
@@ -1,12 +1,39 @@
 // where the bot gives users their colours
+const {Events, EmbedBuilder, } = require('discord.js');
+const UserProfile = require('../schemas/UserProfile.js');
+const ReactionPost = require('../schemas/roleColourData.js');
+
+
 module.exports = (client) => {
+    client.on(Events.MessageReactionAdd, async (reaction, user) => {
 
-    const {Events, EmbedBuilder } = require('discord.js');
-    const ReactionPost = require('../schemas/roleColourData.js');
-    const UserProfile = require('../schemas/UserProfile.js');
+        if (reaction.partial) {
+            try {
+                await reaction.fetch();
+            } catch (error) {
+                console.error('Something went wrong when fetching the reaction: ', error);
+                return;
+            }
+        }
     
-    /* when someone reacts 
-    to a post with 
-    the assigned emojis*/
+        const emoji = reaction.emoji.id ? `<:${reaction.emoji.name}:${reaction.emoji.id}>` : reaction.emoji.name;
+        
+        const ReactionPostData = await ReactionPost.findOne({ 
+            Guild: reaction.message.guild.id, 
+            Message: reaction.message.id, 
+            Emoji: emoji, 
+        });
+    
+        if (!ReactionPostData) {
+            console.log('No matching data found for this reaction.');
+            return;
+        }
 
+        const colourName = ReactionPostData.ColourName;
+        console.log(`${emoji}'s role colour: ${colourName}`); //check what emoji and role colour is being accessed
     
+        const userProfile = await UserProfile.findOne({ userId: user.id });
+    });
+        
+
+}
