73ae01347face1667087c38868fa987de3f931f5
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2024-11-14 16:07:12 -0800

    more cchanges

--- src/commands/capsuleGet.js
+++ src/commands/capsuleGet.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const UserProfile = require('../schemas/UserProfile');
-const newWeightedRandomSelect = require('../functions/ColourWeightsRng.js');
+const newWeightedRandomSelect = require('../functions/colourWeightsRng.js');
 
 const createNewProfile = async (userId) => {
     const newProfile = new UserProfile({
